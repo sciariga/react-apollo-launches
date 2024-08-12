4aa0b6beef2ee1aeb9b8cf731fb7a4caeb9cf742
sciariga <sciarillo.gabriel@gmail.com>
2024-08-12 14:17:52 -0300

    minor change to improve ApolloProvider

--- src/services/ApolloProvider.tsx
+++ src/services/ApolloProvider.tsx
@@ -1,9 +1,21 @@
 import { ApolloClient, InMemoryCache, ApolloProvider as Provider } from '@apollo/client';
 import React from 'react';
 
+const cache = new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          launches: {
+            merge: true,
+          },
+        },
+      },
+    },
+  });
+
 const client = new ApolloClient({
     uri: 'https://apollo-fullstack-tutorial.herokuapp.com/graphql',
-    cache: new InMemoryCache(),
+    cache: cache
 });
 
 export const ApolloProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
